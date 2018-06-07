414ba2509f57b5abd336bf2c7319538992500502
Álvaro García <agarcia@bmat.com>
2018-06-07 18:42:07 +0200

    Test pypi upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='apscheduler_bundle',
     packages=['apscheduler_bundle'],
-    version='1.1',
+    version='1.1.0',
     description='APScheduler support for applauncher',
     author='Alvaro Garcia Gomez',
     author_email='maxpowel@gmail.com',
